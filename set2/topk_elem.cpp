c742fec172977c4c3f9b1af3cb3be017576f5dd1
Hang Shi <hangshiisi@gmail.com>
2018-04-20 20:38:28 -0700

    top k accepted

--- set2/topk_elem.cpp
+++ set2/topk_elem.cpp
@@ -2,6 +2,7 @@
 #include <vector> 
 #include <unordered_map> 
 #include <algorithm> 
+#include <map> 
 
 using namespace std; 
 
@@ -51,40 +52,85 @@ vector<int> topKFrequent(vector<int>& nums, int k) {
 
 
         for (int i = 0; i < result.size(); ++i) { 
-	    cout << " R " << result[i] << " L " << freq[result[i]] << endl; 
-
-	}
+	        cout << " R " << result[i] << " L " << freq[result[i]] << endl; 
+	    }
 
         vector<int> top; 
         top.assign(result.begin(), result.end()-1);
         return top;         
 }
 
-   vector<int> topKFrequent2(vector<int>& nums, int k) {
+
+vector<int> topKFrequent3(vector<int>& nums, int k) {
         vector<int> result(k+1, 0); 
         unordered_map<int, int> freq; 
         
         for (int i = 0; i < nums.size(); i++) { 
             freq[nums[i]]++; 
             result[k] = nums[i]; 
-            int found = k; 
+            cout << "number here "<< nums[i] << " frequency "; 
+            cout << freq[nums[i]] <<endl; 
+            for (int i = 0; i < result.size(); ++i) { 
+                cout << " R " << result[i] << " L " << freq[result[i]] << endl; 
+            }
+ 
             auto it = find(result.begin(), result.end() - 1, result[k]);
-                    
-                
-            for (int j = distance(result.begin(), it) -1; j > 0; --j) { 
-                if (freq[result[j]] > freq[result[j-1]])
-                    swap(result[j], result[j-1]); 
+            int dist = distance(result.begin(), it) - 1;
+            cout<< "Distance   " << dist<<endl; 
+                        
+            for (int j = distance(result.begin(), it) -1; j >= 0; --j) { 
+                if (freq[result[j+1]] > freq[result[j]])
+                    swap(result[j+1], result[j]); 
             }
         }
+
+        cout<<"all finished "<<endl; 
+
+        for (int i = 0; i < result.size(); ++i) { 
+            cout << " R " << result[i] << " L " << freq[result[i]] << endl; 
+        }
+
         vector<int> top; 
         top.assign(result.begin(), result.end()-1);
         return top;         
-    }
+}
+
+
+// Accepted 
+vector<int> topKFrequent4(vector<int>& nums, int k) {        
+        unordered_map<int, int> count;
+        int max_freq = 1;
+        for (const int num : nums)
+            max_freq = max(max_freq, ++count[num]);
+        
+        map<int, vector<int>> buckets;
+        for (const auto& kv : count)
+            buckets[kv.second].push_back(kv.first);
+        
+        vector<int> ans;
+        for (int i = max_freq; i >= 1; --i) {
+            auto it = buckets.find(i);
+            if (it == buckets.end()) continue;
+            ans.insert(ans.end(), it->second.begin(), it->second.end());
+            if (ans.size() == k) return ans;
+        }
+        
+        return ans;  
+}
+
 
 int main() 
 { 
 	vector<int> vect{1,1,1,2,2,3}; 
-	vector<int> v = topKFrequent2(vect, 2); 
+	
+
+    //auto it = find(vect.begin(), vect.end() , 4);
+    //int dist = distance(vect.begin(), it) - 1;
+    //cout << " dist  " << dist << endl; 
+
+    //return 0; 
+
+    vector<int> v = topKFrequent4(vect, 2); 
 
 	for (int i=0; i<v.size(); i++)
         	cout << v[i] << " ";
